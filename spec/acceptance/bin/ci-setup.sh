27c6e5bc4a8c331f77a5f5e821ae022a95578626
Reid Vandewiele <reid@puppetlabs.com>
2015-04-09 10:50:13 -0700

    Add missing ssh-keygen prep step

--- spec/acceptance/bin/ci-setup.sh
+++ spec/acceptance/bin/ci-setup.sh
@@ -9,3 +9,8 @@ if [ ! -z "$AWS_ACCESS_KEY_ID" -a ! -f ~/.fog ]; then
 		  :aws_secret_access_key: $AWS_SECRET_ACCESS_KEY
 	EOF
 fi
+
+# Generate an ssh keypair, if one doesn't already exist
+if [ ! -f ~/.ssh/id_rsa ]; then
+	ssh-keygen -f ~/.ssh/id_rsa -q -N ""
+fi
