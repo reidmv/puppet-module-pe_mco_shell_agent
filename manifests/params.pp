3c96b42db7cb13334af2f5d0f2ff816b90a218ce
Reid Vandewiele <reid@puppetlabs.com>
2015-12-09 16:50:06 -0800

    Use PE libexec dir for Linux mcollective plugins
    The original behavior of the module was to install Linux plugins into
    the PE 3.x /opt/puppet libexec dir. This commit makes the behavior
    remain the same.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,26 +1,22 @@
 class pe_mco_shell_agent::params {
   case $::kernel {
     'windows' : {
-      $libdir  = 'C:/ProgramData/PuppetLabs/mcollective/etc/plugins/mcollective'
       $owner   = undef
       $group   = undef
       $mode    = undef
       $service = undef
+      $libdir  = 'C:/ProgramData/PuppetLabs/mcollective/etc/plugins/mcollective'
     }
     'Linux' : {
       $owner  = 'root'
       $group  = 'root'
       $mode   = '0644'
-
       if versioncmp($::puppetversion, '4') < 0 {
-        $libdir = $::osfamily ? {
-          'Debian' => '/usr/share/mcollective/plugins',
-          default  => '/usr/libexec/mcollective',
-        }
         $service = 'pe-mcollective'
+        $libdir  = '/opt/puppet/libexec/mcollective',
       } else {
-        $libdir = '/opt/puppetlabs/mcollective/plugins'
         $service = 'mcollective'
+        $libdir  = '/opt/puppetlabs/mcollective/plugins'
       }
     }
   }
